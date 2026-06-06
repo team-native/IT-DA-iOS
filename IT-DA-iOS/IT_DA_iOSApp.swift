d9fa03e4528d310a9e8c5fcdaf9b674b9b0b3a72
이시우 <s40409841@gmail.com>
2026-06-07 03:53:46 +0900

    Initial commit

---
+++ IT-DA-iOS/IT_DA_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IT_DA_iOSApp.swift
+//  IT-DA-iOS
+//
+//  Created by 이시우 on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct IT_DA_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
