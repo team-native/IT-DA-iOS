5fac8cb8ec9c92fc926f93486e9061164328956e
안율 <an-yul@an-yul-ui-MacBookPro.local>
2026-08-29 01:15:53 +0900

    feat: 기술 스택 코드 가볍게

--- IT-DA-iOS/Views/Information.swift
+++ IT-DA-iOS/Views/Information.swift
@@ -39,6 +39,7 @@ struct ITDAaddInformation: View {
             VStack(spacing: 0){
                 textField(title: "이름", placeholder: "예: 홍길동, 가나다, 하치와레", text: $name)
                 ITDASelectField(title: "관심 분야", value: "Back-End"){}
+                textField(title: "기술 스택", placeholder: "예: Python, Figma, Swift", text: $techinque)
                 
             }
     }
