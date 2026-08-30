5c866f5055077ebcb8e24bf819740fd8615a4b56
안율 <an-yul@an-yul-ui-MacBookPro.local>
2026-08-30 23:21:02 +0900

    feat: 하치와레 키우기

--- IT-DA-iOS/Views/Home-Screen.swift
+++ IT-DA-iOS/Views/Home-Screen.swift
@@ -18,8 +18,19 @@ struct HomeScreen : View{
         Image("Line 1")
             .padding(.top, 18.5)
         
-        ITDAProjectCard(title: "AI기반 학습 플래너 [0부0부]", subtitle: "백앤드 개발자 1명 모집", chip1: "Back-End")
+        ITDAProjectCard(
+            title: "AI기반 학습 플래너 [0부0부]",
+            subtitle: "백앤드 개발자 1명 모집",
+            chip1: "Back-End"
+        )
             .padding(.horizontal, 26.5)
+        ITDAProjectCard(
+            title: "하치와레 키우기 [하키]",
+            subtitle: "프론트엔드 개발자 2명 모집",
+            chip2: "Back-End"
+        )
+        .padding(.top, 16.5)
+        .padding(.horizontal, 26.5)
     }
 }
 #Preview{
