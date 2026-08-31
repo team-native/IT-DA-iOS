28cfe75cc530fd92c28c99184e6191fee6e0c88d
안율 <an-yul@an-yul-ui-MacBookPro.local>
2026-09-01 01:31:21 +0900

    feat: 알림 글자 폰트 변경 '알림 유형'글 작성 완료

--- IT-DA-iOS/Views/Signal.swift
+++ IT-DA-iOS/Views/Signal.swift
@@ -19,11 +19,16 @@ struct Signal : View {
             .padding(.top, 5.5)
         }
         Text(Signaltext)
-            .font(.custom("Pretendard-Bold",size: 21))
+            .font(.custom("Pretendard-SemiBold",size: 21))
             .frame(maxWidth:.infinity, alignment: .leading)
             .padding(.leading, 28.5)
             .padding(.top, 20.5)
-        
+        Text(Signalforms)
+            .font(.custom("Pretendard-Regular",size: 15))
+            .foregroundStyle(Color("place"))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 28.5)
+            .padding(.top, 14.5)
         
     }
 
