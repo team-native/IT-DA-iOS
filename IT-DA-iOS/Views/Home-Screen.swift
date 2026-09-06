575e6c2e070b544ad0c470d8b1aa360d0ac24a40
안율 <an-yul@an-yul-ui-MacBookPro.local>
2026-09-07 08:38:31 +0900

    feat: scrollview 코드에 추가

--- IT-DA-iOS/Views/Home-Screen.swift
+++ IT-DA-iOS/Views/Home-Screen.swift
@@ -15,51 +15,53 @@ struct HomeScreen : View{
     let chip3 =  ""
     
     var body: some View{
-        Text(upexplanation)
-            .font(.custom("Pretendard-Bold", size: 23))
-        Image("Line 1")
-            .padding(.top, 18.5)
-        
-        ITDAProjectCard(
-            title: "AI기반 학습 플래너 [0부0부]",
-            subtitle: "백앤드 개발자 1명 모집",
-            chip1: "Back-End"
-        )
-            .padding(.horizontal, 26.5)
-        ITDAProjectCard(
-            title: "하치와레 키우기 [하키]",
-            subtitle: "프론트엔드 개발자 2명 모집",
-            chip1: "Back-End"
-        )
-        .padding(.top, 16.5)
-        .padding(.horizontal, 26.5)
-        
-        ITDAProjectCard(
-            title: "닮은 포켓몬 검사 [포켓몬백]",
-            subtitle: "iOS 개발자 한명ㆍ디자이너 1명 모집",
-            status: "마감 입박",
-            chip2: "iOS",
-            chip3: "Design")
-        .padding(.top, 16.5)
-        .padding(.horizontal, 26.5)
-        
-        VStack(spacing: 0){
-            Text(joinproject)
-                .font(.custom("Pretendard-Bold",size: 21))
-                .frame(maxWidth:.infinity,alignment:.leading)
-                .padding(.top,26.5)
-                .padding(.leading, 26.5)
-        }
-        ITDAProjectCard(
-            title: "사랑을 이어주는 앱 [닭발]",
-            subtitle: "내 역할: iOS 개발",
-            status: "진행중"
+        ScrollView{
+            Text(upexplanation)
+                .font(.custom("Pretendard-Bold", size: 23))
+            Image("Line 1")
+                .padding(.top, 18.5)
+            
+            ITDAProjectCard(
+                title: "AI기반 학습 플래너 [0부0부]",
+                subtitle: "백앤드 개발자 1명 모집",
+                chip1: "Back-End"
             )
-        .padding(.horizontal, 26.5)
-        .padding(.top, 11.5)
-        
-        VStack(spacing: 0){
-            Text(alarmcheck)
+            .padding(.horizontal, 26.5)
+            ITDAProjectCard(
+                title: "하치와레 키우기 [하키]",
+                subtitle: "프론트엔드 개발자 2명 모집",
+                chip1: "Back-End"
+            )
+            .padding(.top, 16.5)
+            .padding(.horizontal, 26.5)
+            
+            ITDAProjectCard(
+                title: "닮은 포켓몬 검사 [포켓몬백]",
+                subtitle: "iOS 개발자 한명ㆍ디자이너 1명 모집",
+                status: "마감 입박",
+                chip2: "iOS",
+                chip3: "Design")
+            .padding(.top, 16.5)
+            .padding(.horizontal, 26.5)
+            
+            VStack(spacing: 0){
+                Text(joinproject)
+                    .font(.custom("Pretendard-Bold",size: 21))
+                    .frame(maxWidth:.infinity,alignment:.leading)
+                    .padding(.top,26.5)
+                    .padding(.leading, 26.5)
+            }
+            ITDAProjectCard(
+                title: "사랑을 이어주는 앱 [닭발]",
+                subtitle: "내 역할: iOS 개발",
+                status: "진행중"
+            )
+            .padding(.horizontal, 26.5)
+            .padding(.top, 11.5)
+            
+            VStack(spacing: 0){
+                Text(alarmcheck)
+            }
         }
     }
 }
