dadeddcbcae96d7dd3e272ca1b228ca070ab67dd
안율 <an-yul@an-yul-ui-MacBookPro.local>
2026-09-24 18:14:31 +0900

    feat: Alarm 컴포에서 만들었던 subtitle과 timed을 preview 항목에 호출

--- IT-DA-iOS/Components/Alarm.swift
+++ IT-DA-iOS/Components/Alarm.swift
@@ -16,10 +16,10 @@ struct ITDAAlarm: View{
         VStack(spacing:0){
             Text(subtitle)
                 .font(.custom("Pretendard-Regular", size: 16))
-                .backgroundStyle(Color("dim"))
+                .foregroundStyle(Color("place"))
             Text(time)
                 .font(.custom("Pretendard-Regular", size: 13))
-                .backgroundStyle(Color("dim"))
+                .foregroundStyle(Color("place"))
             
             
         }
@@ -27,8 +27,17 @@ struct ITDAAlarm: View{
         .background(.white)
     }
 }
+
 struct ITDAAlarm_Previews : PreviewProvider{
     static var previews: some View{
-        
+        VStack(spacing:8){
+            ITDAAlarm(
+                subtitle: "지원한 프로젝트에서 새 메시지가 있습니다",
+                time: "2분전"
+            )
+                
+            
+                
+        }
     }
 }
