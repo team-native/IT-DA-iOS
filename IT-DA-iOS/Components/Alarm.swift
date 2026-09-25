7686a767ff7121e39585634d9dc673cc82e5bf08
안율 <an-yul@an-yul-ui-MacBookPro.local>
2026-09-25 17:45:44 +0900

    feat: Alarm 컴포에서 알림요약 및 확인 칸에있는 우편박스 완성

--- IT-DA-iOS/Components/Alarm.swift
+++ IT-DA-iOS/Components/Alarm.swift
@@ -10,34 +10,38 @@ import SwiftUI
 struct ITDAAlarm: View{
     let subtitle : String
     let time : String
-    
-    
     var body : some View {
-        VStack(spacing:0){
-            Text(subtitle)
-                .font(.custom("Pretendard-Regular", size: 16))
-                .foregroundStyle(Color("place"))
-            Text(time)
-                .font(.custom("Pretendard-Regular", size: 13))
-                .foregroundStyle(Color("place"))
+        HStack(spacing: 0){
+            Image("post")
+                .padding(.trailing,15)
             
-            
+            VStack(alignment: .leading,spacing:0){
+                Text(subtitle)
+                    .font(.custom("Pretendard-Regular", size: 16))
+                    .foregroundStyle(Color("place"))
+                    
+                Text(time)
+                    .font(.custom("Pretendard-Regular", size: 13))
+                    .foregroundStyle(Color("place"))
+                    .padding(.top,8)
+                }
+            }
+        .padding(.horizontal, 18)
+            .padding(.vertical, 20)
+            .background(.white)
+            .overlay {
+                RoundedRectangle(cornerRadius: 9)
+                    .stroke(Color("card"), lineWidth: 1)
         }
-        .frame(width: 393,height: 852)
-        .background(.white)
     }
 }
-
-struct ITDAAlarm_Previews : PreviewProvider{
-    static var previews: some View{
+    
+    #Preview{
         VStack(spacing:8){
             ITDAAlarm(
                 subtitle: "지원한 프로젝트에서 새 메시지가 있습니다",
                 time: "2분전"
             )
-                
-            
-                
         }
     }
-}
+
